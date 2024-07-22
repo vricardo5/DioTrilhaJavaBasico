0e20788ae8423f8fbeac6dffed71ad2300bdc277
Vricardo5 <pankosmia2020@outlook.com>
2024-07-22 17:21:36 -0300

    Testando novamente e sem alterações

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,16 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 20;
+
+        System.out.println(numero);
 
     }
 }
